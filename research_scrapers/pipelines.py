df7de26fe2baaf0c2e0904cb402f6eff65b07c0a
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-01-27 19:47:22 -0500

    Added pipeline to export items to files.

--- research_scrapers/pipelines.py
+++ research_scrapers/pipelines.py
@@ -3,10 +3,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
+import os, json
+
 class NanoWrimoPipeline(object):
     def __init__(self):
         pass
 
     def process_item(self, item, spider):
+        f = 'data/' + str(id(item)) + item.__class__.__name__ + '.txt'
+        d = os.path.dirname(f)
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+        item_file = open(f, 'wb')
+        item_text = json.dumps(dict(item), indent=4)
+        item_file.write(item_text)
 
         return item
