8af2879e0db805710155150fbb71988504907814
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-03-22 12:45:02 -0400

    Updated overclocked for new infrastructure. Added new version of overclocked just to scrape normal forums without attempting to go throgh search results. Updated readme according ly.

---
+++ research_scrapers/spiders/overclocked_spider_init.py
@@ -0,0 +1,71 @@
+from scrapy.contrib.spiders.init import InitSpider
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import Rule
+from scrapy.http import FormRequest
+from scrapy.http.request import Request
+
+from spider_helpers.SpiderUtils import make_url_absolute, days_hours_minutes
+from spider_helpers.OverclockedHelper import OverclockedHelper
+
+class OverclockedSpider(InitSpider, OverclockedHelper):
+    name = 'overclocked-init'
+    allowed_domains = ['ocremix.org']
+    login_page = 'http://ocremix.org/forums/index.php'
+    start_urls = ['http://ocremix.org/forums/search.php']
+
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow='forums/search.php?searchid=\d+')),
+        Rule(SgmlLinkExtractor(allow='forums/search.php?searchid=\d+&pp=\d+&page=\d+')),
+        Rule(SgmlLinkExtractor(allow='forums/showthread.php\?t=\d+'), callback='parse_thread'),
+        #Rule(SgmlLinkExtractor(allow='forums/forumdisplay.php\?f=\d+&order=desc&page=\d+')),
+        #Rule(SgmlLinkExtractor(allow='forums/forumdisplay.php\?f=\d+')),
+      )
+
+    def __init__(self):
+        InitSpider.__init__(self)
+        OverclockedHelper.__init__(self)
+
+    def init_request(self):
+        """This function is called before crawling starts."""
+        return Request(url=self.login_page, callback=self.login)
+
+    def login(self, response):
+        """Generate a login request."""
+        return FormRequest.from_response(response,
+                clickdata={'value':'Log in'},
+                formdata={'vb_login_username': 'cwarrior', 'vb_login_password':  'BoeN\NHY'},
+                callback=self.check_login_response)
+
+    def check_login_response(self, response):
+        """Check the response returned by a login request to see if we are
+        successfully logged in.
+        """
+        # check login succeed before going on
+        if "You have entered an invalid username or password." in response.body:
+            self.log("Login failed", level=log.ERROR)
+            return
+        else:
+            self.log("Successfully logged in. Let's start crawling!")
+            # Now the crawling can begin..
+            self.initialized()
+            return Request(url="http://ocremix.org/forums/search.php",
+                      callback=self.search_keywords)
+
+    def search_keywords(self, response):
+        keywords = ('copyright', 'legal', 'illegal', 'permission', 'trademark',
+                    'stealing', 'steal', 'stole', 'license', 'rights', 'attorney',
+                    'infringement', 'copy', 'copying', 'plagiarism')
+
+        for keyword in keywords:
+            yield FormRequest.from_response(response,
+                    clickdata={'name':'dosearch'},
+                    formdata={'query': keyword}, callback=self.text)
+
+    def text(self, response):
+        print "SEARCH RESULTS"
+
+        text_file = open(response.headers['Content-Length'] + '.html', "w+")
+        text_file.write(response.body)
+        text_file.close()
+        
--- research_scrapers/spiders/spider_helpers/OverclockedHelper.py
+++ research_scrapers/spiders/spider_helpers/OverclockedHelper.py
@@ -1,76 +1,61 @@
-from scrapy.selector import HtmlXPathSelector
-from research_scrapers.items import ForumThread, Profile
-from SpiderUtils import check_url
-from datetime import datetime
+from research_scrapers.spiders.spider_helpers.SpiderUtils import safe_list_get, make_url_absolute
+from research_scrapers.spiders.spider_helpers.helper_base import HelperBase
 
-class OverclockedHelper(object):
+from datetime import date, timedelta
 
-    def __init__(self, response):
-        #HelperBase.__init__(self)
-        self.data_key = 'ft'
-        self.hxs = HtmlXPathSelector(response)
-        self.response = response
+class OverclockedHelper(HelperBase):
+
+    def __init__(self):
+        HelperBase.__init__(self)
+        self.time_string = '%m-%d-%Y,%I:%M%p'
 
     def get_posts(self):
         return self.hxs.select("//div[@id='posts']/div")
 
     def load_first_page(self, ft):
         ft['title'] = self.hxs.select("//div[@class='smallfont']/strong/text()").extract()[0]
-        ft['url'] = self.hxs.response.url
+
+        p = self.get_posts()[0]
+        ft['author']= p.select(".//a[@class='bigusername']/text()").extract()[0]
+        
+        ft['url'] = self.response.url
+        ft['forum_name'] = '/'.join(self.hxs.select("//div[@id='breadcrumb']/a/text()").extract())
         ft['responses'] = []
 
     def populate_post_data(self, p):
         fp = {}
 
         comment_body_list = p.select('.//div[@class="forumpost"]/descendant-or-self::*/text()').extract()
-
         # Check if post is empty
         if len(comment_body_list) < 1:
             return fp
 
-        comment_body = "\n Comment: \n"
-        for part in comment_body_list:
-            comment_body += ''.join(part)
-
-        fp['body'] = comment_body
+        fp['body'] = ''.join(comment_body_list)
         fp['author'] = p.select(".//a[@class='bigusername']/text()").extract()[0]
-        date_list = p.select(".//div[@class='normal']/text()").extract()[5].split()
-        date = ''.join(date_list)
-        fp['date'] = date
+
+        post_date = ''.join(p.select(".//div[@class='normal']/text()").extract()[4].split())
+
+        if  'Today' in post_date or 'Yesterday' in post_date:
+            if 'Today' in post_date:
+                day  = date.today()
+            else:
+                day = date.today() - timedelta(1)
+
+            post_date = day.strftime('%m-%d-%Y,') + post_date.split(',')[1]
+
+        fp['date'] = post_date
 
         return fp
 
-    def new_item(self):
-        return ForumThread()
-
-    def next_page(self):
-        prev_link = self.hxs.select('//a[@rel="next"]/@href').extract()
-
-        if len(prev_link) > 0:
-            return True
-
-        return False
-
-    def time_function(self):
-        return lambda x: datetime.strptime(x, '%m-%d-%Y,%I:%M%p')
-
     def prepare_for_processing(self, responses):
-        responses.reverse()
+        #responses.reverse()
+        pass
 
     def get_next_page(self):
-        url, page_num = self.base_url_and_page_number(self.response.url)
-        next_page = url + '&page=' + str(page_num + 1)
+        next_page_url_relative = self.hxs.select("//td[@class='alt2']/following-sibling::td/a/@href").extract()
+        next_page_url_relative = safe_list_get(next_page_url_relative, 0)
 
-        return next_page
+        if next_page_url_relative:
+            return make_url_absolute(self.response, next_page_url_relative)
 
-    def base_url_and_page_number(self, url):
-        split = url.split('&')
-
-        # Check if we are on the first page
-        if len(split) < 2:
-            return url, 1
-
-        base_url, parameter = split
-        page_num = int(parameter.split('=')[1])
-
-        return base_url, page_num
+        return next_page_url_relative
