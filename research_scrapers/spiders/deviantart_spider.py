3cff9dc2a5d9149f8ad8bc3d66194ef4f511f27b
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-03-22 00:41:30 -0400

    Updated deviantart crawler to the new infrastructure. Updated readme. Parsing normal threads with deviantart crawler seems to be working fine.

--- research_scrapers/spiders/deviantart_spider.py
+++ research_scrapers/spiders/deviantart_spider.py
@@ -1,44 +1,19 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.selector import HtmlXPathSelector
-from scrapy.utils.response import get_base_url
-from scrapy.utils.url import urljoin_rfc
-from scrapy.http.request import Request
-from scrapy import log
 
-# from research_scrapers.items import ForumPost, Profile
+from spider_helpers.DeviantArtHelper import DeviantArtHelper
 
-import re
-from datetime import datetime
-
-# TODO Remove unused imports
-
-class DeviantSpider(CrawlSpider):
+class DeviantSpider(CrawlSpider, DeviantArtHelper):
     name = "deviantart"
     allowed_domains = ["deviantart.com"]
-    start_urls = [
-        "https://forum.deviantart.com/"
-    ]
+    start_urls = ["https://forum.deviantart.com/"]
 
-    def parse(self, response):
-        hxs = HtmlXPathSelector(response)
-        forums = hxs.select("//td[@class='f bl']/strong/a/@href").extract()
-        posts = []
-        for forum in forums:
-            print forum
-            f = HtmlXPathSelector(Request(forum))
-            f.select("//td[@class='d-topic f v np']/a/@href").extract()
-            posts.append(f.select("//td[@class='d-topic f v np']/a/@href").extract())
+    rules = (
+    Rule(SgmlLinkExtractor(allow='\w+/\w+/\d+'), callback='parse_thread', follow=False),
+    Rule(SgmlLinkExtractor(allow='\w+/w\+')),
+    Rule(SgmlLinkExtractor(allow='\w+/w\+/\?offset=\d+')),
+  )
 
-        items = []
-        for post in posts:
-            p = HtmlXPathSelector(Request(post))
-            fp = ForumPost()
-            fp['title'] = p.select('//h1/text()').extract()[2]
-            fp['author']  = p.select('/html/body/div[2]/div/div[2]/div/div/div/div/div/span[2]/a/text()').extract()
-            fp['link'] = p.select('a/@href').extract()
-
-            items.append(fp)
-
-
-        return fp
+    def __init__(self):
+        CrawlSpider.__init__(self)
+        DeviantArtHelper.__init__(self)
---
+++ research_scrapers/spiders/spider_helpers/DeviantArtHelper.py
@@ -0,0 +1,48 @@
+from research_scrapers.spiders.spider_helpers.SpiderUtils import safe_list_get, make_url_absolute
+from research_scrapers.spiders.spider_helpers.helper_base import HelperBase
+
+class DeviantArtHelper(HelperBase):
+
+    def __init__(self):
+        HelperBase.__init__(self)
+        # 'Mar 21, 2013, 6:42:57 PM'
+        self.time_string = '%b %d, %Y, %I:%M:%S %p'
+
+    def get_posts(self):
+        return self.hxs.select("//div[@class='ctext ch']")
+
+    def load_first_page(self, ft):
+        ft['title'] =  self.hxs.select("//div[@class='forum-header']/h1/text()").extract()[0]
+
+        p = self.get_posts()[0]
+        ft['author'] = p.select(".//a[@class='u']/text()").extract()[0]
+
+        ft['url'] = self.response.url
+        ft['forum_name'] = '/'.join(self.hxs.select("//div[@class='catpath']/a/text()").extract())
+        ft['responses'] = []
+
+    def populate_post_data(self, p):
+        fp = {}
+        fp['body'] =  ''.join(p.select(".//div[@class='text text-ii']/descendant-or-self::*/text()").extract())
+        fp['author'] = p.select(".//a[@class='u']/text()").extract()[0]
+        date = p.select(".//span[@class='cc-time']/a/@title").extract()[0]
+
+        if 'ago' in date:
+            time = date.split('at ')[1][:-1]
+            date = p.select(".//span[@class='cc-time']/a/text()").extract()[0] + ', ' + time
+
+        fp['date'] = date
+
+        return fp
+
+    def prepare_for_processing(self, responses):
+        pass
+
+    def get_next_page(self):
+        next_page_url_relative = self.hxs.select("//li[@class='next']/a[@class='away']/@href").extract()
+        next_page_url_relative = safe_list_get(next_page_url_relative, 0)
+
+        if next_page_url_relative:
+            return make_url_absolute(self.response, next_page_url_relative)
+
+        return next_page_url_relative
