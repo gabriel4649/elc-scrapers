b7cb945e1df407a3599c805b3066eacd4d6f278a
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-04-28 12:58:59 -0400

    Allows urls from all domains.

--- manual-search-scrape/get_urls.js
+++ manual-search-scrape/get_urls.js
@@ -1,6 +1,5 @@
 var urls= [];
 for (var i= document.links.length; i-->0;)
-    if (document.links[i].hostname===location.hostname)
         urls.push(document.links[i].href);
 
 function copyToClipboard (text) {
