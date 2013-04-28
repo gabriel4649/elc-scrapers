ad1fef19745a5643a20d1485a4fa16d6194a08a1
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-04-28 19:09:31 -0400

    Updates url grabbing scripts and the urls

--- manual-search-scrape/prepare_for_py.sh
+++ manual-search-scrape/prepare_for_py.sh
@@ -5,4 +5,4 @@ egrep -oh "http://ocremix.org/forums/showthread\.php\?[tp]=[0-9]*" * | sort | un
 egrep -oh "http://forums.fanart-central.net/viewtopic\.php\?[pt]=[0-9]*" * | sort | uniq | sed -e 's/.*/\"&\",/' > results; echo "]" >> results; sed -i '1iurls= [' results
 
 # Deviantart
-egrep -oh "http://forum.deviantart.com/[A-Za-z]*/[A-Za-z]*/[0-9]*" * | sort | uniq | sed -e 's/.*/\"&\",/' > results; echo "]" >> results; sed -i '1iurls= [' results
+egrep -oh "http://forum.deviantart.com/[A-Za-z]+/[A-Za-z]+/[0-9]+/?" * | sort | uniq | sed -e 's/.*/\"&\",/' > results; echo "]" >> results; sed -i '1iurls= [' results
