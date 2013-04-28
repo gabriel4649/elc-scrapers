72c68211464558ab96c2c8049c547cad094008db
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-04-28 16:29:51 -0400

    Updates url preparation commands and the urls.

--- manual-search-scrape/prepare_for_py.sh
+++ manual-search-scrape/prepare_for_py.sh
@@ -1,1 +1,8 @@
-grep -h "thread" * | grep "page" -v | sort | uniq | sed -e 's/.*/\"&\",/' > results; echo "]" >> results; sed -i '1iurls= [' results
+# Overclocked
+egrep -oh "http://ocremix.org/forums/showthread\.php\?[tp]=[0-9]*" * | sort | uniq | sed -e 's/.*/\"&\",/' > results; echo "]" >> results; sed -i '1iurls= [' results
+
+# Fanart
+egrep -oh "http://forums.fanart-central.net/viewtopic\.php\?[pt]=[0-9]*" * | sort | uniq | sed -e 's/.*/\"&\",/' > results; echo "]" >> results; sed -i '1iurls= [' results
+
+# Deviantart
+egrep -oh "http://forum.deviantart.com/[A-Za-z]*/[A-Za-z]*/[0-9]*" * | sort | uniq | sed -e 's/.*/\"&\",/' > results; echo "]" >> results; sed -i '1iurls= [' results
