94ff09767122bfd4f4a903b8c785055d37276090
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-04-29 14:26:39 -0400

    Adds window id parameter to scroll script.

--- manual-search-scrape/youtube_scroll.sh
+++ manual-search-scrape/youtube_scroll.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+wid="$1"
+
 for (( i=1; i<=100; i++ ))
 do
     NUMBER=$[ ( $RANDOM % 10 )  + 5 ]
-    sleep $NUMBER; xdotool key space
+    sleep $NUMBER; xdotool key space $wid
 done
