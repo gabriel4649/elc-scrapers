78ceec7f50c7da14178ea94d71b2e729b1a50fbe
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-04-29 15:36:25 -0400

    Fixes problem with HP scraper getting stuck.

--- research_scrapers/spiders/hpfanfic_spider.py
+++ research_scrapers/spiders/hpfanfic_spider.py
@@ -6,13 +6,55 @@ from spider_helpers.HPFanFicHelper import HPFanFicHelper
 class HPFanFicSpider(CrawlSpider, HPFanFicHelper):
     name = 'hpfanfic'
     allowed_domains = ['hpfanfictionforums.com']
-    start_urls = ['http://www.hpfanfictionforums.com/index.php']
+
+    # All the forums and sub-forums in:
+    # http://www.hpfanfictionforums.com/index.php?act=idx
+    start_urls = \
+       ["http://www.hpfanfictionforums.com/index.php?showforum=2",
+        "http://www.hpfanfictionforums.com/index.php?showforum=10",
+        "http://www.hpfanfictionforums.com/index.php?showforum=115",
+        "http://www.hpfanfictionforums.com/index.php?showforum=281",
+        "http://www.hpfanfictionforums.com/index.php?showforum=160",
+        "http://www.hpfanfictionforums.com/index.php?showforum=14",
+        "http://www.hpfanfictionforums.com/index.php?showforum=135",
+        "http://www.hpfanfictionforums.com/index.php?showforum=169",
+        "http://www.hpfanfictionforums.com/index.php?showforum=360",
+        "http://www.hpfanfictionforums.com/index.php?showforum=7",
+        "http://www.hpfanfictionforums.com/index.php?showforum=1",
+        "http://www.hpfanfictionforums.com/index.php?showforum=3",
+        "http://www.hpfanfictionforums.com/index.php?showforum=6",
+        "http://www.hpfanfictionforums.com/index.php?showforum=44",
+        "http://www.hpfanfictionforums.com/index.php?showforum=43",
+        "http://www.hpfanfictionforums.com/index.php?showforum=45",
+        "http://www.hpfanfictionforums.com/index.php?showforum=273",
+        "http://www.hpfanfictionforums.com/index.php?showforum=138",
+        "http://www.hpfanfictionforums.com/index.php?showforum=4",
+        "http://www.hpfanfictionforums.com/index.php?showforum=119",
+        "http://www.hpfanfictionforums.com/index.php?showforum=19",
+        "http://www.hpfanfictionforums.com/index.php?showforum=193",
+        "http://www.hpfanfictionforums.com/index.php?showforum=206",
+        "http://www.hpfanfictionforums.com/index.php?showforum=267",
+        "http://www.hpfanfictionforums.com/index.php?showforum=9",
+        "http://www.hpfanfictionforums.com/index.php?showforum=112",
+        "http://www.hpfanfictionforums.com/index.php?showforum=30",
+        "http://www.hpfanfictionforums.com/index.php?showforum=29",
+        "http://www.hpfanfictionforums.com/index.php?showforum=165",
+        "http://www.hpfanfictionforums.com/index.php?showforum=385",
+        "http://www.hpfanfictionforums.com/index.php?showforum=166",
+        "http://www.hpfanfictionforums.com/index.php?showforum=147",
+        "http://www.hpfanfictionforums.com/index.php?showforum=148",
+        "http://www.hpfanfictionforums.com/index.php?showforum=149",
+        "http://www.hpfanfictionforums.com/index.php?showforum=150",
+        "http://www.hpfanfictionforums.com/index.php?showforum=152",
+        "http://www.hpfanfictionforums.com/index.php?showforum=142"]
 
     rules = (
-        Rule(SgmlLinkExtractor(allow='showtopic=\d+', deny='page=\d+'), callback='parse_thread', follow=False),
-        Rule(SgmlLinkExtractor(allow='showforum=\d+&prune_day=100&sort_by=Z-A&sort_key=last_post&topicfilter=all&page=\d+')),
-        Rule(SgmlLinkExtractor(allow='showforum=\d+')),
-      )
+        Rule(SgmlLinkExtractor(allow=r'showtopic=\d+', deny=r'page=\d+'), \
+             callback=r'parse_thread', follow=False),
+        Rule(SgmlLinkExtractor( \
+                                allow=r'showforum=\d+&prune_day=100&sort_by=Z-A&sort_key=last_post&topicfilter=all&page=\d+')),
+        #Rule(SgmlLinkExtractor(allow=r'showforum=\d+', deny=('sort'))),
+    )
 
     def __init__(self):
         CrawlSpider.__init__(self)
