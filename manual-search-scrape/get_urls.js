310d003f63689af5f412d22b716691f14d2e44f0
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-09-14 14:48:56 -0400

    Now saves urls in js.

--- manual-search-scrape/get_urls.js
+++ manual-search-scrape/get_urls.js
@@ -1,9 +1,22 @@
-var urls= [];
-for (var i= document.links.length; i-->0;)
-        urls.push(document.links[i].href);
+var urls = [];
 
-function copyToClipboard (text) {
+function getLinks(urls) {
+    for (var i= document.links.length; i-->0;) {
+        var link = document.links[i].href;
+        var index = urls.indexOf(link);
+        if (index == -1) {
+            console.log("Inserting:" + link);
+            urls.push(link);
+        }
+    }
+
+    return;
+}
+
+function copyToClipboard(text) {
     window.prompt ("Copy to clipboard: Ctrl+C, Enter", text);
 }
 
-copyToClipboard(urls)
+//copyToClipboard(urls);
+
+setInterval("getLinks(urls)", 1000);
